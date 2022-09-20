aef816bca5edc514e25a48e9f1bbcd8f364ead04
somethingGit <113336038+somethingGit@users.noreply.github.com>
2022-09-20 12:16:19 -0600

    Improvments

--- Interactive_Scene_Assignment/sketch.js
+++ Interactive_Scene_Assignment/sketch.js
@@ -5,6 +5,10 @@
 // Extra for Experts:
 //I decided to add the mouse wheel as a way of zooming in and zooming out.
 
+let scrollbutton = 0;
+let diameter = 50;
+let x = windowWidth()/2;
+let y = windowHeight()/2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,9 +16,13 @@ function setup() {
 
 function draw() {
   background(220);
-  
+  circle(x, y, diameter + scrollbutton);
 }
 
 function mouseClicked() {
-  square(mouseX - 50 / 2, mouseY - 50 / 2, 50);
+  square(mouseX - 50 / 2, mouseY - 50 / 2, 50 + scrollbutton);
+}
+
+function mouseWheel(event) {
+  scrollbutton += event;
 }
