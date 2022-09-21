b7c43dc5306f56eab079f8308549878756055d2d
somethingGit <113336038+somethingGit@users.noreply.github.com>
2022-09-21 11:56:13 -0600

    Changed File Location, Small Improvements to code

--- Interactive_Scene_Assignment/sketch.js
+++ Interactive_Scene_Assignment/sketch.js
@@ -8,21 +8,26 @@
 let scrollbutton = 0;
 let diameter = 50;
 let x = windowWidth()/2;
-let y = windowHeight()/2;
+let y = 50;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
-  circle(x, y, diameter + scrollbutton);
+  //background(220);
+  fill("yellow");
+  circle(windowWidth / 2, windowHeight / 2, diameter + scrollbutton);
+  if(mouseIsPressed === true) {
+    fill(0);
+    square(mouseX - 5 / 2, mouseY - 5 / 2, 5);
+  }
 }
 
 function mouseClicked() {
-  square(mouseX - 50 / 2, mouseY - 50 / 2, 50 + scrollbutton);
+  
 }
 
 function mouseWheel(event) {
-  scrollbutton += event;
+  scrollbutton += event.delta;
 }
