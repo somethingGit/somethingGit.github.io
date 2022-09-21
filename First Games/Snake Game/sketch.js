afcd7c57d20ab70f692adcd89100c2b8c615edf7
somethingGit <113336038+somethingGit@users.noreply.github.com>
2022-09-21 12:15:13 -0600

    Last Update for now

---
+++ First Games/Snake Game/sketch.js
@@ -0,0 +1,26 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+}
+
+function snakeOutOfBounds() {
+  if(snakeX > windowWidth || snakeY > windowHeight || snakeX < 0 || snakeY < 0) {
+    backToBeginning();
+  }
+}
+
+function backToBeginning() {
+  snake 
+}
