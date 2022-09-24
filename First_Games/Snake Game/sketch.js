165af8579b5bd1470b667f82bd6e87f17e695c5c
somethingGit <113336038+somethingGit@users.noreply.github.com>
2022-09-24 17:25:02 -0600

    Adds Fruits

--- First_Games/Snake Game/sketch.js
+++ First_Games/Snake Game/sketch.js
@@ -21,11 +21,25 @@ var lastDirectionX = 1;
 // eslint-disable-next-line no-var
 var lastDirectionY = 0;
 
+let headPosition = [7, 0];
+
+let hasMoved = true;
+
+let fruitX;
+
+let fruitY;
+
+let thereIsFruit = false;
+
+let gridCoordinatesX = [];
+
+let gridCoordinatesY = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(1);
-  savedSnakeLocationX = [2,1,0];
-  savedSnakeLocationY = [0,0,0];
+  frameRate(2);
+  savedSnakeLocationX = [0,1,2,3,4,5,6];
+  savedSnakeLocationY = [0,0,0,0,0,0,0];
   fixTurn = savedSnakeLocationX.length;
 }
 
@@ -39,7 +53,7 @@ function makeGrid() {
   let totalSquaresHeight = 20;//prompt("Please input height: ");
   let gridWidth = windowWidth / totalSquaresWidth;
   let gridHeight = windowHeight / totalSquaresHeight;
-  let snakeLength = savedSnakeLocationX.length;
+  let snakeLength = savedSnakeLocationX.length + 1;
   gridWidth = windowWidth / totalSquaresWidth;
   gridHeight = windowHeight / totalSquaresHeight;
   for(let x = 0; x < width; x += gridWidth) {
@@ -48,124 +62,129 @@ function makeGrid() {
       stroke(160);
       fill(255);
       rect(x, y, gridWidth, gridHeight);
+      append(gridCoordinatesX, x);
+      append(gridCoordinatesY, y);
     }
   }
-  makeSnake(gridWidth, gridHeight);
+  //makeSnake(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight);
+  snakeOutOfBounds(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight);
 }
 
-function makeSnake(gridWidth, gridHeight) {
+function snakeOutOfBounds(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight) {
+  for(let i = 0; i < savedSnakeLocationX.length + 1; i++) {
+    if((savedSnakeLocationX * gridWidth > windowWidth || savedSnakeLocationX * gridWidth < 0 || savedSnakeLocationY * gridHeight > windowHeight || savedSnakeLocationY * gridHeight < 0) || headPosition[0] === savedSnakeLocationX[i] && headPosition[1] === savedSnakeLocationY[i]) {
+      backToBeginning();
+    }
+  }
+  makeSnake(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight);
+}
+
+function makeSnake(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight) {
   fill("green");
-  rect(headPosition * gridWidth)
+  rect(headPosition[0] * gridWidth, headPosition[1] * gridHeight, gridWidth, gridHeight);
   for(let i = 0; i < savedSnakeLocationX.length; i++) {
     fill("green");
     rect(savedSnakeLocationX[i] * gridWidth, savedSnakeLocationY[i] * gridHeight, gridWidth, gridHeight);
   }
-  normalMove(gridWidth, gridHeight);
+  addFruit(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight);
 }
 
-function normalMove(gridWidth, gridHeight) {
-  /*
+function addFruit(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight) {
+  if(thereIsFruit === false) {
+    fruitX = random(gridCoordinatesX);
+    fruitY = random(gridCoordinatesY);
+    thereIsFruit = true;
+  }
+  console.log(fruitX);
+  console.log(fruitY);
+  snakeEatFruit(gridWidth, gridHeight);
+}
+
+function snakeEatFruit(gridWidth, gridHeight) {
+  if(headPosition[0] === fruitX && headPosition[1] === fruitY) {
+    thereIsFruit = false;
+    console.log("They touched eachother");
+  }
+  drawFruit(gridWidth, gridHeight);
+}
+
+function drawFruit(gridWidth, gridHeight) {
+  if(thereIsFruit === true) {
+    fill("red");
+  }
+  else if(thereIsFruit === false) {
+    fill("white");
+  }
+  rect(fruitX, fruitY, gridWidth, gridHeight);
+  normalMove();
+}
+
+function normalMove() {
+  append(savedSnakeLocationX, headPosition[0]);
+  reverse(savedSnakeLocationX);
+  shorten(savedSnakeLocationX);
+  reverse(savedSnakeLocationX);
+  append(savedSnakeLocationY, headPosition[1]);
+  reverse(savedSnakeLocationY);
+  shorten(savedSnakeLocationY, 0, 1);
+  reverse(savedSnakeLocationY);
   if(goX === 1) {
-    for(let i = 0; i < fixTurn; i++) {
-      savedSnakeLocationX[i] = savedSnakeLocationX[i] + 1;
-    }
+    headPosition[0] = headPosition[0] + 1;
+    hasMoved = true;
   }
   else if(goX === -1) {
-    for(let i = 0; i < fixTurn; i++) {
-      savedSnakeLocationX[i] = savedSnakeLocationX[i] - 1;
-      console.log(savedSnakeLocationX[i]);
-    }
+    headPosition[0] = headPosition[0] - 1;
+    hasMoved = true;
   }
   else if(goY === 1) {
-    for(let i = 0; i < fixTurn; i++) {
-      savedSnakeLocationY[i] = savedSnakeLocationY[i] - 1;
-    }
+    headPosition[1] = headPosition[1] - 1;
+    hasMoved = true;
   }
   else if(goY === -1) {
-    for(let i = 0; i < fixTurn; i++) {
-      savedSnakeLocationY[i] = savedSnakeLocationY[i] + 1;
-    }
-  }
-  
-  
-  if(lastDirectionX === 1 && fixTurn < savedSnakeLocationX.length) {
-    for(let i = 0; i < savedSnakeLocationX.length - fixTurn; i++) {
-      savedSnakeLocationX[savedSnakeLocationX - i] = savedSnakeLocationX[savedSnakeLocationX - i] + 1;
-    }
-  }
-  else if(lastDirectionX === -1 && fixTurn < savedSnakeLocationX.length) {
-    for(let i = 0; i < savedSnakeLocationX.length - fixTurn; i++) {
-      savedSnakeLocationX[savedSnakeLocationX - i] = savedSnakeLocationX[savedSnakeLocationX - i] - 1;
-    }
-  }
-  else if(lastDirectionY === 1 && fixTurn < savedSnakeLocationX.length) {
-    for(let i = 0; i < savedSnakeLocationX.length - fixTurn; i++) {
-      savedSnakeLocationY[savedSnakeLocationX - i] = savedSnakeLocationY[savedSnakeLocationX - i] + 1;
-    }
-  }
-  else if(lastDirectionY === -1 && fixTurn < savedSnakeLocationX.length) {
-    for(let i = 0; i < savedSnakeLocationX.length - fixTurn; i++) {
-      savedSnakeLocationY[savedSnakeLocationX - i] = savedSnakeLocationY[savedSnakeLocationX - i] - 1;
-    }
-  }
-
-  if(fixTurn < savedSnakeLocationX.length) {
-    fixTurn++;
-  }
-  else if(fixTurn === savedSnakeLocationX.length) {
-    lastDirectionX = goX;
-    lastDirectionY = goY;
-  }
-  console.log(fixTurn);
-  */
- lastDirectionX.push(headPosition);
-}
-
-function snakeOutOfBounds(snakeX, snakeY) {
-  if(snakeX > windowWidth || snakeY > windowHeight || snakeX < 0 || snakeY < 0) {
-    backToBeginning(snakeX);
-  }
-  for(let i = 0; i < savedSnakeLocationX.length; i++) {
-    backToBeginning(snakeX);
-  }
-  for(let i = 0; i < savedSnakeLocationY.length; i++) {
-    backToBeginning(snakeX);
+    headPosition[1] = headPosition[1] + 1;
+    hasMoved = true;
   }
 }
 
-function backToBeginning(snakeX) {
-  savedSnakeLocationX = [2,1,0];
-  savedSnakeLocationY = [0,0,0];
+
+function backToBeginning() {
+  savedSnakeLocationX = [0,1,2,3,4,5,6];
+  savedSnakeLocationY = [0,0,0,0,0,0,0];
+  headPosition = [7, 0];
 }
 
 function keyPressed() {
   switch(keyCode) {
   case RIGHT_ARROW:
-    if(goX !== -1) {
+    if(goX !== -1 && hasMoved) {
       goX = 1;
       goY = 0;
       fixTurn = 0;
+      hasMoved = false;
     }
     break;
   case LEFT_ARROW:
-    if(goX !== 1) {
+    if(goX !== 1 && hasMoved) {
       goX = -1;
       goY = 0;
       fixTurn = 0;
+      hasMoved = false;
     }
     break;
   case UP_ARROW:
-    if(goY !== -1) {
+    if(goY !== -1 && hasMoved) {
       goX = 0;
       goY = 1;
       fixTurn = 0;
+      hasMoved = false;
     }
     break;
   case DOWN_ARROW:
-    if(goY !== 1) {
+    if(goY !== 1 && hasMoved) {
       goX = 0;
       goY = -1;
       fixTurn = 0;
+      hasMoved = false;
     }
     break;
   default:
@@ -174,5 +193,7 @@ function keyPressed() {
 }
 
 function windowResized() {
+  let gridCoordinatesX = [];
+  let gridCoordinatesY = [];
   resizeCanvas(windowWidth, windowHeight);
 }
