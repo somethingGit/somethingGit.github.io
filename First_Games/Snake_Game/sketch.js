e92cdb9c93185edc8001498d3ed2e42043eafa63
somethingGit <113336038+somethingGit@users.noreply.github.com>
2022-09-27 00:36:11 -0600

    Some Code Improvements

--- First_Games/Snake_Game/sketch.js
+++ First_Games/Snake_Game/sketch.js
@@ -5,28 +5,24 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let savedSnakeLocationX = [];
-let savedSnakeLocationY = [];
-let goX = 1;
-let goY = 0;
+let savedSnakeLocationX = [], savedSnakeLocationY = [];
+let goX = 1, goY = 0;
 let fixTurn;
-let lastDirectionX = 1;
-let lastDirectionY = 0;
-let headPosition = [7, 0];
+let lastDirectionX = 1, lastDirectionY = 0;
+let headPosition = [2, 0];
 let hasMoved = true;
-let fruitX;
-let fruitY;
+let fruitX, fruitY;
 let thereIsFruit = false;
-let gridCoordinatesX = [];
-let gridCoordinatesY = [];
-let totalSquaresWidth = 40;//prompt("Please input width: ");
-let totalSquaresHeight = 40;//prompt("Please input height: ");
+let gridCoordinatesX = [], gridCoordinatesY = [];
+let totalSquaresWidth = 40, totalSquaresHeight = 40;
+let snakeLength = 0;
+let score = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(5);
-  savedSnakeLocationX = [0,1,2,3,4,5,6];
-  savedSnakeLocationY = [0,0,0,0,0,0,0];
+  frameRate(10);
+  savedSnakeLocationX = [0,1];
+  savedSnakeLocationY = [0,0];
   fixTurn = savedSnakeLocationX.length;
 }
 
@@ -38,7 +34,7 @@ function draw() {
 function makeGrid() {
   let gridWidth = windowWidth / totalSquaresWidth;
   let gridHeight = windowHeight / totalSquaresHeight;
-  let snakeLength = savedSnakeLocationX.length + 1;
+  snakeLength = savedSnakeLocationX.length + 1;
   gridWidth = windowWidth / totalSquaresWidth;
   gridHeight = windowHeight / totalSquaresHeight;
   gridCoordinatesX = [];
@@ -53,18 +49,9 @@ function makeGrid() {
       gridCoordinatesY.push(y);
     }
   }
-  //makeSnake(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight);
-  snakeOutOfBounds(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight);
-}
-
-function snakeOutOfBounds(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight) {
-  for(let i = -1; i < savedSnakeLocationX.length + 1; i++) {
-    if(headPosition[0] > gridCoordinatesX.length || headPosition[0] < 0 || headPosition[1]  > gridCoordinatesY.length - 2 || headPosition[1] < 0 || headPosition[0] === savedSnakeLocationX[i] && headPosition[1] === savedSnakeLocationY[i]) {
-      backToBeginning();
-    }
-  }
   makeSnake(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight);
 }
+
 
 function makeSnake(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight) {
   fill("green");
@@ -78,18 +65,20 @@ function makeSnake(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight)
 
 function addFruit(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight) {
   if(thereIsFruit === false) {
-    fruitX = round(random(totalSquaresWidth));
-    fruitY = round(random(totalSquaresHeight));
-    thereIsFruit = true;
+    randomFruitLocation();
   }
-  drawFruit(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight);
+  drawFruit(gridWidth, gridHeight);
 }
 
 
 function drawFruit(gridWidth, gridHeight) {
   fill("red");
+  if(fruitX * gridWidth > width || fruitY * gridHeight > height) {
+    addFruit();
+  }
   rect(fruitX * gridWidth, fruitY * gridHeight, gridWidth, gridHeight);
   normalMove(gridWidth, gridHeight);
+  console.log(fruitX, width, fruitY, height);
 }
 
 function normalMove(gridWidth, gridHeight) {
@@ -113,13 +102,19 @@ function normalMove(gridWidth, gridHeight) {
     headPosition[1] = headPosition[1] + 1;
     hasMoved = true;
   }
+  addScore(gridWidth, gridHeight);
+}
+
+function addScore(gridWidth, gridHeight) {
+  let textPx = 32;
+  textSize(textPx);
+  text(score - 1, headPosition[0] * gridWidth + textPx / 2, headPosition[1] * gridHeight + textPx / 2);
   snakeEatFruit(gridWidth, gridHeight);
 }
 
-function snakeEatFruit(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHeight) {
+function snakeEatFruit(gridWidth, gridHeight) {
   if(headPosition[0] === fruitX && headPosition[1] === fruitY) {
     thereIsFruit = false;
-    console.log(thereIsFruit);
     reverse(savedSnakeLocationX);
     append(savedSnakeLocationX, savedSnakeLocationX[savedSnakeLocationX.length] - gridWidth);
     reverse(savedSnakeLocationX);
@@ -127,41 +122,60 @@ function snakeEatFruit(gridWidth, gridHeight, totalSquaresWidth, totalSquaresHei
     append(savedSnakeLocationY, savedSnakeLocationY[savedSnakeLocationY.length] - gridHeight);
     reverse(savedSnakeLocationY);
   }
+  snakeOutOfBounds(gridWidth, gridHeight);
+}
+
+function snakeOutOfBounds(gridWidth, gridHeight) {
+  for(let i = -1; i < savedSnakeLocationX.length + 1; i++) {
+    if(headPosition[0] * gridWidth > width || headPosition[0] < 0 || headPosition[1] * gridHeight > height || headPosition[1] < 0 || headPosition[0] === savedSnakeLocationX[i] && headPosition[1] === savedSnakeLocationY[i]) {
+      backToBeginning();
+    }
+  }
+  console.log("Position of the head", headPosition[0] * gridWidth, width);
 }
 
 function backToBeginning() {
-  savedSnakeLocationX = [0,1,2,3,4,5,6];
-  savedSnakeLocationY = [0,0,0,0,0,0,0];
-  headPosition = [7, 0];
+  savedSnakeLocationX = [0,1];
+  savedSnakeLocationY = [0,0];
+  headPosition = [2, 0];
   goX = 1;
   goY = 0;
+  score = 0;
   thereIsFruit = false;
 }
 
-function between(x, min, max) {
-  return x >= min && x <= max;
+function randomFruitLocation() {
+  fruitX = round(random(totalSquaresWidth)) - 1;
+  fruitY = round(random(totalSquaresHeight)) - 1;
+  score++;
+  for(let i = 0; i < snakeLength; i++) {
+    if(fruitX === headPosition[i]) {
+      randomFruitLocation();
+    }
+  }
+  thereIsFruit = true;
 }
 
 function keyPressed() {
   switch(keyCode) {
-  case RIGHT_ARROW:
-    if(goX !== -1 && hasMoved) {
-      goX = 1;
-      goY = 0;
-      fixTurn = 0;
-      hasMoved = false;
-    }
-    break;
-  case LEFT_ARROW:
-    if(goX !== 1 && hasMoved) {
-      goX = -1;
-      goY = 0;
-      fixTurn = 0;
-      hasMoved = false;
-    }
-    break;
-  case UP_ARROW:
-    if(goY !== -1 && hasMoved) {
+    case RIGHT_ARROW:
+      if(goX !== -1 && hasMoved) {
+        goX = 1;
+        goY = 0;
+        fixTurn = 0;
+        hasMoved = false;
+      }
+      break;
+      case LEFT_ARROW:
+        if(goX !== 1 && hasMoved) {
+          goX = -1;
+          goY = 0;
+          fixTurn = 0;
+          hasMoved = false;
+        }
+        break;
+        case UP_ARROW:
+          if(goY !== -1 && hasMoved) {
       goX = 0;
       goY = 1;
       fixTurn = 0;
