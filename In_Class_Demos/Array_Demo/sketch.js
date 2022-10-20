b54485567c4f69b925f2578626781e83107ab1d9
somethingGit <113336038+somethingGit@users.noreply.github.com>
2022-10-20 12:18:32 -0600

    Saved Stuff

--- In_Class_Demos/Array_Demo/sketch.js
+++ In_Class_Demos/Array_Demo/sketch.js
@@ -8,13 +8,26 @@
 let twoNumbers = [1, -1];
 let circleArray = [];
 let diameter = 50;
+let setDeltaTime;
+let fr = 60;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(fr);
+  // eslint-disable-next-line no-undef
+  setDeltaTime = deltaTime;
 }
 
 function draw() {
   background(220);
+  console.log(fr);
+  if(setDeltaTime < deltaTime && fr > 1) {
+    fr--;
+    console.log(`Framerate has slown ${fr}`);
+  }
+  else if(setDeltaTime > deltaTime) {
+    fr++;
+  }
   makeCircle();
 }
 
@@ -25,15 +38,25 @@ function makeCircle() {
       circle(circleArray[i], circleArray[i + 1], diameter);
       circleArray[i] += circleArray[i + 6];
       circleArray[i+1] += circleArray[i + 7];
-      if(circleArray[i] >= width - diameter / 2 || circleArray[i] <= 0 + diameter / 2) {
+      if(circleArray[i] >= width - diameter / 2 || circleArray[i] <= 0 + diameter / 2 || circleArray[i] === mouseX) {
         circleArray[i+6] *= -1;
       }
-      if(circleArray[i+1] >= height - diameter / 2 || circleArray[i + 1] <= 0 + diameter / 2) {
+      if(circleArray[i+1] >= height - diameter / 2 || circleArray[i + 1] <= 0 + diameter / 2 || circleArray[i+1] === mouseY) {
         circleArray[i + 7] *= -1;
       }
     }
   }
+  if(mouseIsPressed) {
+    circleArray.push(mouseX);
+    circleArray.push(mouseY);
+    circleArray.push(random(255), random(255), random(255), random(255));
+    circleArray.push(random(twoNumbers));
+    circleArray.push(random(twoNumbers));
+  }
+  makeMouseOverCircle();
 }
+
+let makeMouseOverCircle = () => circle(mouseX, mouseY, 30);
 
 function mousePressed() {
   circleArray.push(mouseX);
