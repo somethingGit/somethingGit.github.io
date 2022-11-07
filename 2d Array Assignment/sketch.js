f2f5af0b628657118cdf71ea7b279dd0b88cb929
somethingGit <113336038+somethingGit@users.noreply.github.com>
2022-11-06 23:54:28 -0600

    Improved Mouse Hovering

--- 2d Array Assignment/sketch.js
+++ 2d Array Assignment/sketch.js
@@ -21,9 +21,9 @@ function setup() {
 }
 
 function makeGrid(totalSquares, squaresSize) {
-  for(let x = 0; x <= height - squaresSize; x += squaresSize) {
-    for(let y = 0; y <= height - squaresSize; y += squaresSize) {
-      grid.push([x, y, 0]);
+  for(let x = 0; x < sqrt(totalSquares); x++) {
+    for(let y = 0; y < sqrt(totalSquares); y++) {
+      grid.push([x * squaresSize, y * squaresSize, 0]);
     }
   }
 }
@@ -31,8 +31,12 @@ function makeGrid(totalSquares, squaresSize) {
 function draw() {
   background(220);
   displayGrid();
-  mouseMoved();
+  if(shouldCheck()) {
+    tokenDisplay();
+  }
 }
+
+let shouldCheck = () => mouseX >= grid[8][0] && mouseY >= grid[8][1] && mouseX < grid[grid.length-1 - 8][0] && mouseY < grid[grid.length - 1 - 8][0];
 
 function displayGrid() {
   for(let i = 0; i < grid.length; i++) {
@@ -49,21 +53,55 @@ function displayGrid() {
   }
 }
 
-function mouseMoved() {
+function tokenDisplay() {
   for(let i = grid.length - 1; i > 0; i--) {
-    if(mouseX >= grid[i][0] && mouseY >= grid[i][1] && mouseX < grid[i][0] + squaresSize && mouseY < grid[i][1] + squaresSize && (grid[i-1] === -1 || grid[i-1] === 1)) {
+    if(shouldDisplay(i) && playerIsThere(i)) {
       fill("red");
       circle(grid[i][0] + squaresSize / 2, grid[i][1] + squaresSize / 2, squaresSize / 2);
     }
   }
 }
 
+let shouldDisplay = (i) => mouseX >= grid[i][0] && mouseY >= grid[i][1] && mouseX <= grid[i][0] + squaresSize && mouseY <= grid[i][1] + squaresSize && (grid[i - 1][2] === player * -1 && grid[i][2] !== player || grid[i - 8][2] === player * -1 && grid[i][2] !== player || grid[i + 8][2] === player * -1 && grid[i][2] !== player || grid[i + 1][2] === player * -1 && grid[i][2] !== player);
+
+function playerIsThere(x) {
+  let num = multipleOfEight(x);
+  for(let i = num; i < num + sqrt(totalSquares); i++) {
+    if(grid[i] === player) {
+      return true;
+    }
+    console.log(grid[i] === player);
+  }
+  return false;
+}
+
+function multipleOfEight(x) {
+  while(x % 8 !== 0) {
+    x--;
+    if(x % 8 === 0) {
+      return x;
+    }
+  }
+  return x;
+}
+
+function repairOnWindowSizeChange() {
+  let i = 0;
+  squaresSize = height / sqrt(totalSquares);
+  for(let x = 0; x < sqrt(totalSquares); x++) {
+    for(let y = 0; y < sqrt(totalSquares); y++) {
+      grid.push([x * squaresSize, y * squaresSize, grid[i][2]]);
+      i++;
+    }
+  }
+  grid.reverse();
+  for(let j = 0; j < totalSquares; j++) {
+    grid.pop();
+  }
+  grid.reverse();
+}
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  grid = [];
-  makeGrid(totalSquares, squaresSize);
-  grid[27][2] = 1;
-  grid[28][2] = -1;
-  grid[35][2] = -1;
-  grid[36][2] = 1;
+  repairOnWindowSizeChange();
 }
