a011eafc823741757d67b0b50933dbafec5cc4fe
somethingGit <113336038+somethingGit@users.noreply.github.com>
2022-11-22 11:16:08 -0600

    Very Short and Efficient Code

---
+++ Test Code.js
@@ -0,0 +1,5 @@
+let factorial=n=>(n===0)||(n===1)?(n===0)?0:1:n*factorial(n - 1);console.log(factorial(5));
+
+let fibonacci = n =>(n===0)||(n===1)?(n===0)?0:1:fibonacci(n-1)+fibonacci(n-2);console.log(fibonacci(7));
+
+
