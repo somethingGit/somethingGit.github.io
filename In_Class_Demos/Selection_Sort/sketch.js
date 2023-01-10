7a1729e3c894a3cff0793721d90c66fe80d0bd8e
somethingGit <113336038+somethingGit@users.noreply.github.com>
2023-01-10 04:15:52 -0600

    Selection Sort Does Not Yet Work

---
+++ In_Class_Demos/Selection_Sort/sketch.js
@@ -0,0 +1,27 @@
+// Selection Sort
+// Jason
+// 01/10/2023
+
+let someArray = [5, 15, 3, 8, 9, 1, 20, 7];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  selectionSort(someArray);
+}
+
+function selectionSort(list) {
+  let largest = 0;
+  let listAmount = 1; 
+  for(let i = 0; i < list.length - 1; i++) {
+    for(let j = 0;  j < list.length - listAmount; j++) {
+      if(list[i] > list[largest]) {
+        largest = i;
+      }
+    }
+    list.push(list[largest]);
+    list.splice(list[largest], 1);
+    listAmount++;
+    largest = 0;
+  }
+  return list;
+}
